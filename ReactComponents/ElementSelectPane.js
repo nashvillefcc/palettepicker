8ffa774f37523f0cb7b0929b0436decd8dbfdedd
Tyler Watts <tylermwatts@gmail.com>
2019-02-24 10:25:27 -0600

    wip

---
+++ ReactComponents/ElementSelectPane.js
@@ -0,0 +1,29 @@
+"use strict";
+
+class ElementSelect extends React.Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      selectedElement: null
+    };
+    this.handleClick = this.handleClick.bind(this);
+  }
+
+  handleClick(button) {
+    this.setState({ selectedElement: button });
+  }
+
+  render() {
+    const { buttons } = props;
+    return (
+      <div id="select-buttons-container">
+        {buttons.forEach(b => {
+          <button id={b.buttonName} onClick={this.handleClick(b.buttonName)}>
+            {b.buttonName}
+          </button>;
+        })}
+        ;
+      </div>
+    );
+  }
+}
---
+++ ReactComponents/PreviewPane.js
@@ -0,0 +1,5 @@
+'use strict';
+
+class PreviewPane extends React.Component {
+  
+}
