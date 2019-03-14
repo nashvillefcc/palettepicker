1083625660a9e5e81c7f49786674975b255f5c3b
Warpfox <tylermwatts@gmail.com>
2019-03-13 21:33:26 -0500

    alert on sign-in/sign-up click

--- src/Components/NavHeader/NavHeader.js
+++ src/Components/NavHeader/NavHeader.js
@@ -1,5 +1,9 @@
 import React from "react";
 
+const doAlert = () => {
+  return alert("This button don't actually do nothin'");
+};
+
 const NavHeader = () => {
   return (
     <nav className="navbar navbar-expand-lg navbar-light bg-light">
@@ -7,7 +11,9 @@ const NavHeader = () => {
       <div className="collapse navbar-collapse">
         <ul className="navbar-nav mr-auto">
           <li className="nav-item">
-            <button className="nav-link">Sign In/Sign Up</button>
+            <button className="nav-link" onClick={doAlert}>
+              Sign In/Sign Up
+            </button>
           </li>
         </ul>
       </div>
