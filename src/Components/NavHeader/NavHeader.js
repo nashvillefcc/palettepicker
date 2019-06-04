7f15bd64426f75ed5e6c64208eafebdbf78c38de
Tyler <tylermwatts@gmail.com>
2019-06-04 17:56:52 -0400

    stylin' ain't easy

--- src/Components/NavHeader/NavHeader.js
+++ src/Components/NavHeader/NavHeader.js
@@ -1,10 +1,21 @@
-import React from "react";
+import React from 'react';
 
 const NavHeader = () => {
   return (
-    <nav className="navbar navbar-expand-lg navbar-light bg-light">
+    <nav
+      style={{
+        display: 'flex',
+        flexDirection: 'row',
+        justifyContent: 'center'
+      }}
+      className="navbar navbar-expand-lg navbar-light bg-light"
+    >
       <h3 className="navbar-brand">Palette Picker</h3>
-      <span>{"Use the color selection boxes below, or get a pregenerated palette from Coolors.co"}</span>
+      <span>
+        {
+          'Use the color selection boxes below, or get a pregenerated palette from Coolors.co'
+        }
+      </span>
     </nav>
   );
 };
